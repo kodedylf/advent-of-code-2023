457cc36ce157ccb5ffd9167f1445133b2c2e5b54
Hans Kilian <hans@kilianconsult.dk>
2023-12-03 16:45:12 +0100

    Day 3 part 2

--- 03/Program.cs
+++ 03/Program.cs
@@ -4,7 +4,73 @@ class Program
     static void Main(string[] args)
     {
         var data = ReadData("data.txt");
-        DoPart1(data);
+        // DoPart1(data);
+        DoPart2(data);
+    }
+
+    private static void DoPart2(string[] data) {
+        Dictionary<int, List<int>> adjacentNumbersByGear = new Dictionary<int, List<int>>();
+        FindGears(adjacentNumbersByGear, data);
+        FindNumbersAdjacentToGears(adjacentNumbersByGear, data);
+        int sum = 0;
+        foreach (var kvp in adjacentNumbersByGear) {
+            if (kvp.Value.Count == 2) {
+                sum += kvp.Value[0] * kvp.Value[1];
+            }
+        }
+        Console.WriteLine(sum);
+    }
+
+    private static void FindGears(Dictionary<int, List<int>> adjacentNumbersByGear, string[] data) {
+        for (int i = 0; i < data.Length; i++) {
+            var line = data[i];
+            for (int ix = 0; ix < line.Length; ix++) {
+                if (line[ix] == '*') {
+                    adjacentNumbersByGear[i * 1000 + ix] = new List<int>();
+                }
+            }
+        }
+    }
+
+    private static void FindNumbersAdjacentToGears(Dictionary<int, List<int>> adjacentNumbersByGear, string[] data) {
+        for (int i = 0; i < data.Length; i++) {
+            var line = data[i];
+            int ix = 0;
+            int number = 0;
+            HashSet<int> adjacentGears = new HashSet<int>();
+            while (ix < line.Length) {
+                if (char.IsNumber(line[ix])) {
+                    number = number * 10 + int.Parse("" + line[ix]);
+                    FindAdjacentGears(adjacentGears, data, i, ix);
+                } 
+                if ((!char.IsNumber(line[ix])) || (ix == line.Length-1)) {
+                    if (number != 0) {
+                        foreach (var gear in adjacentGears) {
+                            adjacentNumbersByGear[gear].Add(number);
+                        }
+                    }
+                    number = 0;
+                    adjacentGears = new HashSet<int>();
+                }
+                ix++;
+            }
+        }
+    }
+
+    private static void FindAdjacentGears(HashSet<int> gears, string[] data, int y, int x) {
+        if (PositionIsGear(data, y-1, x-1)) gears.Add((y-1)*1000 + (x-1));
+        if (PositionIsGear(data, y-1, x)) gears.Add((y-1)*1000 + x);
+        if (PositionIsGear(data, y-1, x+1)) gears.Add((y-1)*1000 + (x+1));
+        if (PositionIsGear(data, y, x-1)) gears.Add(y*1000 + (x-1));
+        if (PositionIsGear(data, y, x+1)) gears.Add(y*1000 + (x+1));
+        if (PositionIsGear(data, y+1, x-1)) gears.Add((y+1)*1000 + (x-1));
+        if (PositionIsGear(data, y+1, x)) gears.Add((y+1)*1000 + x);
+        if (PositionIsGear(data, y+1, x+1)) gears.Add((y+1)*1000 + (x+1));
+    }
+
+    private static bool PositionIsGear(string[] data, int y, int x) {
+        if ((y < 0) || (x < 0) || (y >= data.Length) || (x >= data[0].Length)) return false;
+        return (data[y][x] == '*');
     }
 
     private static void DoPart1(string[] data) {
